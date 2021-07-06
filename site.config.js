65e6ebbaecf5f076305d6781565066ded7a74c2b
biglight <you@example.com>
2021-07-06 11:30:32 +0800

    修改评论库

--- site.config.js
+++ site.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
   // social usernames (optional)
   twitter: '',
-  github: '',
+  github: 'biglight-h',
   linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
