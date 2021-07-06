f5bbbbbf66eab25ca05b6395307bb8b0f5cfca9d
biglight <you@example.com>
2021-07-06 16:20:52 +0800

    修改图标

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: 'dc2c38ffa2f04aae81a2cade158a2ade?v=e495b1b789d344308cd38ebaf23ea2e7',
+  rootNotionPageId: '84c4b85559ce4a049bc71a49ef28e01a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -8,7 +8,7 @@ module.exports = {
 
   // basic site info (required)
   name: 'BIG LIGHT',
-  domain: 'https://www.notion.so/dc2c38ffa2f04aae81a2cade158a2ade?v=e495b1b789d344308cd38ebaf23ea2e7',
+  domain: 'https://www.notion.so/BIG-LIGHT-84c4b85559ce4a049bc71a49ef28e01a',
   author: 'BIG LIGHT',
 
   // open graph metadata (optional)
